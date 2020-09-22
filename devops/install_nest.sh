83ade3e037d38a25c82250c03b025a00252918a9
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-22 23:26:27 +0200

    oops, wasn't succesfully cloning the dbbs fork

--- devops/install_nest.sh
+++ devops/install_nest.sh
@@ -3,11 +3,11 @@ echo "Empty cache, starting NEST v$NEST_VERSION installation."
 echo ""
 export MY_BEFORE_DIR=$PWD
 cd /home/travis
-git clone --depth 1 --branch v$NEST_VERSION https://github.com/dbbs-lab/nest-simulator nest-simulator-$NEST_VERSION
+git clone --depth 1 https://github.com/dbbs-lab/nest-simulator nest-simulator-$NEST_VERSION
 mkdir nest-simulator-$NEST_VERSION-build
 mkdir nest-install-$NEST_VERSION
 cd nest-simulator-$NEST_VERSION-build
-cmake -Dwith-python=3 -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m/ -DCMAKE_INSTALL_PREFIX:PATH=/home/travis/nest-$NEST_VERSION /home/travis/nest-simulator-$NEST_VERSION
+cmake -Dwith-python=3 -DPYTHON_EXECUTABLE=/home/travis/ -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m/ -DCMAKE_INSTALL_PREFIX:PATH=/home/travis/nest-$NEST_VERSION /home/travis/nest-simulator-$NEST_VERSION
 make
 make install
 cd $MY_BEFORE_DIR
