23bcf3e07343c4e48d8b3099ef0e5f2f8d245727
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-22 23:28:27 +0200

    split up the command for readability

--- devops/install_nest.sh
+++ devops/install_nest.sh
@@ -7,7 +7,12 @@ git clone --depth 1 https://github.com/dbbs-lab/nest-simulator nest-simulator-$N
 mkdir nest-simulator-$NEST_VERSION-build
 mkdir nest-install-$NEST_VERSION
 cd nest-simulator-$NEST_VERSION-build
-cmake -Dwith-python=3 -DPYTHON_EXECUTABLE=/home/travis/ -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m/ -DCMAKE_INSTALL_PREFIX:PATH=/home/travis/nest-$NEST_VERSION /home/travis/nest-simulator-$NEST_VERSION
+cmake -Dwith-python=3 \
+  -DPYTHON_EXECUTABLE=/home/travis/ \
+  -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so \
+  -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m/ \
+  -DCMAKE_INSTALL_PREFIX:PATH=/home/travis/nest-$NEST_VERSION \
+  /home/travis/nest-simulator-$NEST_VERSION
 make
 make install
 cd $MY_BEFORE_DIR
