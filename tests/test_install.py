83507147a80b629fd4eeda1645298a5345e7ba7e
Robin De Schepper <robin.deschepper93@gmail.com>
2020-09-23 23:34:04 +0200

    use LD_LIBRARY_PATH

--- tests/test_install.py
+++ tests/test_install.py
@@ -1,13 +1,1 @@
 import os, nest
-
-def list_files(startpath):
-    print("Listing files for", startpath)
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
-
-list_files(os.path.sep.join(nest.__path__[0].split(os.path.sep)[:-4]))
